2b12c8b84f840c1264cbb2994438277250308a75
thalaskn <149984653+thalaskn@users.noreply.github.com>
2024-05-09 04:06:18 +0530

    Add files via upload

---
+++ ASS 2/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#btnAddStudent").click(function () {
+        function getStudentData() {
+            let student = {
+                firstName: $("#firstName").val(),
+                lastName: $("#lastName").val(),
+                gender: $("input[name='gender']:checked").val(),
+                college: $("#college").val()
+            };
+            return student;
+        }
+        function storeDataToLocalStorage() {
+            if (!localStorage.getItem("student")) {
+                localStorage.setItem("student", JSON.stringify(getStudentData()));
+            } else {
+                localStorage.removeItem("student");
+                localStorage.setItem("student", JSON.stringify(getStudentData()));
+            }
+        }
+        storeDataToLocalStorage();
+        window.location.href="display.html"
+    });
+});
